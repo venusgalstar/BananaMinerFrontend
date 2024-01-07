a97b575379d9635a65395d5241908947ae492998
nenad27dev <nenad.dev.ra@gmail.com>
2024-01-07 07:48:07 +0000

    exchange initial project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-// import 'font-awesome/css/font-awesome.min.css';
-import './App.css';
-import Interface from './components';
+import { useMemo } from "react";
+import Box from "@mui/material/Box";
+import { BrowserRouter } from "react-router-dom";
+import Home from "./Home";
 
-const App = () => {
-return (
-<>
-<Interface />
-</>
+import { Wallets } from './components/wallet'
 
-);
+import { SnackbarProvider } from 'notistack';
+
+function App() {
+
+// I will try to start with new project
+// this project is so stressful
+  return (
+    <BrowserRouter>
+      <Box paddingY={6} paddingX={2}>
+        <SnackbarProvider>
+          <Wallets>
+            <Home />
+          </Wallets>
+        </SnackbarProvider>
+      </Box>
+    </BrowserRouter>
+  );
 }
 
 export default App;
